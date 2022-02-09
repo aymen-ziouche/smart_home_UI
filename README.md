e775af6dba43f99e544894ec705a79c15246a6da
aymen.ziouche <aymen.zch23@gmail.com>
2022-02-09 01:15:51 +0100

    init commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# smart_home_app
+
+UI for Smart Home App
+
+## Getting Started
+
+<img src="https://user-images.githubusercontent.com/46304263/153091089-21005eab-e262-4f78-85d7-a8ed4fdea8e3.png" width="45%"></img> <img src="https://user-images.githubusercontent.com/46304263/153091097-a944e226-d00f-40a3-afa2-688e24552dc5.png" width="45%"></img>
---
+++ lib/main.dart
@@ -0,0 +1,170 @@
+import 'package:flutter/material.dart';
+import 'package:smart_home_app/temperature.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Smart Home App',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.indigo.shade50,
+      body: SafeArea(
+        child: Container(
+          margin: const EdgeInsets.only(top: 18, left: 24, right: 24),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  Text(
+                    "HI THERE",
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.indigo,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  RotatedBox(
+                    quarterTurns: 135,
+                    child: Icon(
+                      Icons.bar_chart_rounded,
+                      size: 28,
+                      color: Colors.indigo,
+                    ),
+                  ),
+                ],
+              ),
+              Expanded(
+                child: ListView(
+                  physics: const BouncingScrollPhysics(),
+                  children: [
+                    const SizedBox(
+                      height: 32,
+                    ),
+                    Center(
+                      child: Image.asset(
+                        "assets/images/banner.png",
+                        scale: 1.2,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 32,
+                    ),
+                    const Center(
+                      child: Text(
+                        "Smart Home",
+                        style: TextStyle(
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 48),
+                    const Text(
+                      "SERVICES",
+                      style:
+                          TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        _cardMenu(
+                            title: "ENERGY", icon: "assets/images/energy.png"),
+                        _cardMenu(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => TemperaturePage()));
+                          },
+                          title: "TEMPERATURE",
+                          icon: "assets/images/temperature.png",
+                          fontColor: Colors.white,
+                          color: Colors.indigoAccent,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        _cardMenu(
+                            title: "WATER", icon: "assets/images/water.png"),
+                        _cardMenu(
+                            title: "ENTERTAINMENT",
+                            icon: "assets/images/entertainment.png"),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _cardMenu({
+    required String title,
+    required String icon,
+    VoidCallback? onTap,
+    Color color = Colors.white,
+    Color fontColor = Colors.grey,
+  }) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.symmetric(
+          vertical: 36,
+        ),
+        width: 156,
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(24),
+        ),
+        child: Column(
+          children: [
+            Image.asset(icon),
+            const SizedBox(height: 10),
+            Text(
+              title,
+              style: TextStyle(fontWeight: FontWeight.bold, color: fontColor),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
